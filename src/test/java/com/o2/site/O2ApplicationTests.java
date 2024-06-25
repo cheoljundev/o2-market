c284f59a32b828b7cebe7513bfec76cb62d692b8
cheoljundev <cheoljundev@gmail.com>
2024-06-26 02:05:58 +0900

    fix: @SpringBootTest(classes = O2Application.class) 추가

--- src/test/java/com/o2/site/O2ApplicationTests.java
+++ src/test/java/com/o2/site/O2ApplicationTests.java
@@ -1,9 +1,11 @@
 package com.o2.site;
 
+import com.o2.site.config.O2Application;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootTest
+@SpringBootTest(classes = O2Application.class)
 class O2ApplicationTests {
 
     @Test
