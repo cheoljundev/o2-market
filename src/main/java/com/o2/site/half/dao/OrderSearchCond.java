ac3bf2a6c22ee73930237610968b5cb37ebeb3a0
cheoljundev <cheoljundev@gmail.com>
2024-06-26 16:42:54 +0900

    fix : OrderSearchCond.java 애노테이션 추가

--- src/main/java/com/o2/site/half/dao/OrderSearchCond.java
+++ src/main/java/com/o2/site/half/dao/OrderSearchCond.java
@@ -1,10 +1,14 @@
 package com.o2.site.half.dao;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderSearchCond {
     private String buyerMemberId;
     private String buyerPhone;
