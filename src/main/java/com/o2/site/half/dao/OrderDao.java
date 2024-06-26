9ec069fac58da1193fe74da5c8192db80125429f
cheoljundev <cheoljundev@gmail.com>
2024-06-26 15:49:02 +0900

    fix: 불필요한 @Param 제거

--- src/main/java/com/o2/site/half/dao/OrderDao.java
+++ src/main/java/com/o2/site/half/dao/OrderDao.java
@@ -3,14 +3,13 @@ package com.o2.site.half.dao;
 import com.o2.site.half.domain.Order;
 import com.o2.site.half.dto.InsertOrderDto;
 import com.o2.site.half.dto.UpdateOrderDto;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface OrderDao {
     Order findByOrderNo(Long orderNo);
     List<Order> findAll(OrderSearchCond orderSearchCond);
-    List<Order> findRange(@Param("start") int start, @Param("end") int end, @Param("orderSearchCond") OrderSearchCond orderSearchCond);
+    List<Order> findRange(int start, int end, OrderSearchCond orderSearchCond);
     void insertOrder(InsertOrderDto insertOrderDto);
     void updateOrder(UpdateOrderDto updateOrderDto);
     void deleteOrder(Long orderNo);
