517a1ff283e793b1426121590131fb9c6f76104a
cheoljundev <cheoljundev@gmail.com>
2024-06-26 16:43:23 +0900

    feat: 검색 폼을 통해 검색 조건 넘기기

--- src/main/java/com/o2/site/half/controller/HalfAdminController.java
+++ src/main/java/com/o2/site/half/controller/HalfAdminController.java
@@ -1,20 +1,30 @@
 package com.o2.site.half.controller;
 
+import com.o2.site.half.dao.OrderSearchCond;
+import com.o2.site.half.domain.Order;
+import com.o2.site.half.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequestMapping("/admin/half")
+@RequiredArgsConstructor
 public class HalfAdminController {
 
-    @GetMapping
-    public String index() {
-        return "/half/admin/order";
-    }
+    private final OrderService orderService;
 
-    @GetMapping("/order")
-    public String order() {
+    @GetMapping({"", "/", "/order" })
+    public String order(@ModelAttribute OrderSearchCond orderSearchCond, Model model) {
+        List<Order> orders = orderService.findAll(orderSearchCond);
+        model.addAttribute("orders", orders);
         return "/half/admin/order";
     }
 }
