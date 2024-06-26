323aabf944ff6b0f524da49e9fde5f1eab07c9f8
ajw <anjy0821@naver.com>
2024-06-26 17:01:17 +0900

    상세 페이지부분 구현 이미지 넘기기, 조회수, 찜추가, 게시글 삭제

--- src/main/java/com/o2/site/trade/controller/TradeController.java
+++ src/main/java/com/o2/site/trade/controller/TradeController.java
@@ -1,8 +1,7 @@
 package com.o2.site.trade.controller;
 
-import com.o2.site.trade.dto.ApplicationDto;
-import com.o2.site.trade.dto.SearchDto;
-import com.o2.site.trade.dto.TradeMainDto;
+import com.o2.site.trade.domain.TradeDomain;
+import com.o2.site.trade.dto.*;
 import com.o2.site.trade.service.TradeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,7 +20,7 @@ public class TradeController {
 
     @Autowired
     TradeService tradeService;
-    //전체 리스트
+    //전체 리스트 관리자 페이지 구성 후 approve 조건 추가 예정
     @GetMapping("/trade_main")
     public void trade_main(Model model){
         ArrayList<TradeMainDto> mainlist = tradeService.selectMainList();
@@ -33,21 +32,57 @@ public class TradeController {
     //검색 리스트
     @GetMapping("/trade_search")
     public String trade_sarch(Model model, SearchDto searchDto){
+        if(searchDto.getCategory().equals("cg_all")){
+            searchDto.setCategory("");
+        }
         ArrayList<TradeMainDto> searchList = tradeService.selectSearchList(searchDto);
         System.out.println(searchList);
-        if(searchDto.getCategory()==""){
-            searchDto.setCategory("전체");
-        }
-        String cg = searchDto.getCategory();
-        System.out.println(cg);
         model.addAttribute("mainlist", searchList);
-        model.addAttribute("cg",cg);
+        if(tradeService.getCg(searchDto)==null){
+            model.addAttribute("cg","전체");
+        }else {
+        model.addAttribute("cg",tradeService.getCg(searchDto));
+        }
         return "/trade/trade_main";
     }
     @GetMapping("/trade_application")
     public void trade_application(){}
+    //게시글 한개 조회
     @GetMapping("/trade_detail")
-    public void trade_detail(){}
+    public void trade_detail(Model model, String tradeNo){
+        TradeDomain tradeDomain = tradeService.getBoard(tradeNo);
+        System.out.println(tradeDomain);
+        tradeService.upVisitCount(tradeNo);
+        ArrayList<String> imageList = tradeService.getImages(tradeNo);
+        System.out.println(imageList);
+        int memberNo=1;//이후 로그인한 멤버번호로 변경 예정
+        WishListDto wishListDto = tradeService.getWishCount(memberNo,tradeNo);
+        if(wishListDto==null){
+            model.addAttribute("wishCount",0);
+        }else {
+            model.addAttribute("wishCount",wishListDto.getCount());
+        }
+        model.addAttribute("tradeDomain",tradeDomain);
+        model.addAttribute("imageList",imageList);
+    }
+    //삭제
+    @GetMapping("/trade_delete")
+    public String rade_delete(String tradeNo){
+        int result = tradeService.deleteBoard(tradeNo);
+        if(result!=0){
+            System.out.println("삭제 성공");
+        }else {
+            System.out.println("삭제 실패");
+        }
+        return "redirect:/trade/trade_main";
+    }
+    //찜하기
+    @GetMapping("/trade_addWish")
+    public String trade_addWish(WishListDto wishListDto){
+        wishListDto.setMemberNo(1);
+        int result = tradeService.addWish(wishListDto);
+        return "redirect:/trade/trade_detail?tradeNo="+wishListDto.getTradeNo();
+    }
     @GetMapping("/trade_admin")
     public void trade_admin(){}
     @GetMapping("/trade_admin_application")
---
+++ src/main/java/com/o2/site/trade/dto/WishListDto.java
@@ -0,0 +1,14 @@
+package com.o2.site.trade.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class WishListDto {
+    private int memberNo;
+    private int tradeNo;
+    private int count;
+}
