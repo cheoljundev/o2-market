1cff811d3e02db19c58c214bb1280f08b47f9ecb
cheoljundev <cheoljundev@gmail.com>
2024-06-27 16:38:32 +0900

    feat: 주문정보 detail 조회

--- src/main/java/com/o2/site/half/controller/HalfAdminController.java
+++ src/main/java/com/o2/site/half/controller/HalfAdminController.java
@@ -2,16 +2,14 @@ package com.o2.site.half.controller;
 
 import com.o2.site.half.dao.OrderSearchCond;
 import com.o2.site.half.domain.Order;
+import com.o2.site.half.dto.AdminOrderDetailDto;
 import com.o2.site.half.dto.AdminOrderListDto;
 import com.o2.site.half.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -60,5 +58,21 @@ public class HalfAdminController {
         model.addAttribute("searchField", searchField);
         model.addAttribute("searchValue", searchValue);
         return "/half/admin/order";
+    }
+
+    @ResponseBody
+    @PostMapping("/order")
+    public AdminOrderDetailDto detail(@RequestBody Long orderNo) {
+        Order order = orderService.findByOrderNo(orderNo);
+        return AdminOrderDetailDto.builder()
+                .orderNo(order.getOrderNo())
+                .createAt(order.getCreateAt())
+                .title(order.getTitle())
+                .recipientName(order.getRecipientName())
+                .recipientPhone(order.getRecipientPhone())
+                .recipientAddress(order.getRecipientAddress())
+                .deliveryMemo(order.getDeliveryMemo())
+                .invoice(order.getInvoice())
+                .build();
     }
 }
