2e8379c547f9b9f6ced4ba0213ffaf35f292bfd8
ajw <anjy0821@naver.com>
2024-06-28 17:16:14 +0900

    테스트 코드

--- src/main/java/com/o2/site/trade/domain/AdvDomain.java
+++ src/main/java/com/o2/site/trade/domain/AdvDomain.java
@@ -1,5 +1,6 @@
 package com.o2.site.trade.domain;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -7,6 +8,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Builder
 public class AdvDomain {
     private int advNo;
     private String title;
