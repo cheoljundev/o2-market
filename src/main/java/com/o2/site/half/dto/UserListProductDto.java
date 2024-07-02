c052e9955ed200040c0dc55071b8b4cedcf15abc
cheoljundev <cheoljundev@gmail.com>
2024-07-03 02:51:20 +0900

    fix : UserListProductDto에 포맷 추가

--- src/main/java/com/o2/site/half/dto/UserListProductDto.java
+++ src/main/java/com/o2/site/half/dto/UserListProductDto.java
@@ -2,6 +2,8 @@ package com.o2.site.half.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
 
 import java.util.Date;
 
@@ -13,9 +15,12 @@ public class UserListProductDto {
     private Long sellerMemberNo;
     private String sellerMemberId;
     private String sellerPhone;
+    @NumberFormat(pattern = "#,###")
     private Long price;
+    @NumberFormat(pattern = "#,###")
     private Long halfPrice;
     private Integer isDone;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date createdAt;
     private String adminMemo;
     private String categoryCode;
